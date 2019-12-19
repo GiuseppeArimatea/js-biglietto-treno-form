4834c9011ebd16d23833c255d5f031cec3404008
giuseppe <Giuseppearimatea10@gmail.com>
2019-12-19 20:07:25 +0100

    js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,36 @@
 // percorrere e l’età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
-
-
-
 // chiedere il numero km e età
-var numeroKm = parseInt(prompt('inserisci i Km'));
-console.log(numeroKm);
-var etaPasseggero = parseInt(prompt('inserisci la tua età'));
-console.log(etaPasseggero);
 
-// Calcolare Costo biglietto
-var prezzoBiglietto = numeroKm * 0.21;
+var buttonGenera = document.getElementById('genera'); // creiamo una variabile che selezione il bottone Genera
+buttonGenera.addEventListener ('click', // Assegniamo i parametri dopo il click
+  function() {
+    var nomeInput = document.getElementById('nome-utente');  // parametro Utente
+    var nome = nomeInput.value; // assegnamo alla variabile il value inserito da tastiera
+    console.log(nome);
 
-if (isNaN(numeroKm) || isNaN(etaPasseggero)) {
-  alert ('non hai inserito un numero');
-  } else {
-      if (etaPasseggero < 18) {
-       console.log(prezzoBiglietto - (prezzoBiglietto * 20) / 100);}
-   else if (etaPasseggero >= 65){
-      console.log(prezzoBiglietto - (prezzoBiglietto * 40) / 100);
-  } else
-    console.log(prezzoBiglietto);
-  }
-}
+    var inputKm = document.getElementById('km');  // parametro KM
+    var numeroKm = parseInt(inputKm.value); // assegnamo alla variabile il value inserito da tastiera
+    console.log(numeroKm);
+
+    var fasciaEtaSelect = document.getElementById('fascia-eta'); // paremetro Eta
+    var fascia = fasciaEtaSelect.value; // assegnamo alla variabile il value inserito da tastiera
+    console.log(fascia);
+
+    var prezzoBiglietto = numeroKm * 0.21;
+    var offerta = 'tariffa standard'
+
+    if (fascia == 'minorenne') {
+        prezzoBiglietto -= ((prezzoBiglietto * 20) / 100);
+        offerta = 'sconto minorenne';
+    } else if (fascia == 'over65') {
+        prezzoBiglietto = ((prezzoBiglietto * 40) / 100);
+        offerta = 'sconto over65';
+    }
+
+    document.getElementById('nome-passeggero').innerHTML = nome;
+    document.getElementById('offerta').innerHTML = offerta;
+    document.getElementById('costo-biglietto').innerHTML = prezzoBiglietto.toFixed(2);
+    }
+);
